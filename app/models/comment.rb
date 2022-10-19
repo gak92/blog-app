c850c649639d2de71dab300e768b47c82136812b
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-19 20:34:02 +0500

    Add methods for the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, counter_cache: true
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
+
 end
