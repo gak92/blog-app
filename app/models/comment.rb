efe8e2a00608f3e7a2f7265d5947189c77c1e236
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-19 22:04:41 +0500

    Fix custom methods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post, counter_cache: true
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
+  after_save :update_comments_counter
 
   def update_comments_counter
     post.increment!(:comments_counter)
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
   has_many :comments
   has_many :likes
+  after_save :update_posts_counter
 
   def update_posts_counter
     author.increment!(:posts_counter)
