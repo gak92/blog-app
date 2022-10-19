e180f26832a751d63b57b97cf1243bdb6d7c33a2
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-19 20:34:20 +0500

    Add methods for the like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
-  belongs_to :post
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post, counter_cache: true
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id', counter_cache: true
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+  end
+
 end
