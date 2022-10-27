e56c6dc20729b5d92079b557eaa1bcaa0fa6ac88
gak92 <44802922+gak92@users.noreply.github.com>
2022-10-27 13:42:45 +0500

    Add test cases for the posts requests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,34 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
-      get '/posts/index'
-      expect(response).to have_http_status(:success)
+    before(:each) do
+      get '/users/1/posts'
+    end
+
+    it 'Users index is successful' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Renders template correctly' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'Response should include right placeholder text' do
+      expect(response.body).to include('List of all post for a given user')
+    end
+  end
+
+  describe 'Show single user details' do
+    before(:each) do
+      get '/users/1/posts/1'
+    end
+    it 'Response will be successful.' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'Template render correctly' do
+      expect(response).to render_template(:show)
+    end
+    it 'Response should include right placeholder text.' do
+      expect(response.body).to include('Post details will be shown here')
     end
   end
 end
