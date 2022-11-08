71540db737670ca073075a167c8c1303509f3987
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-08 15:41:59 +0500

    Solve n plus 1 query problem using eager loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find_by(id: params[:user_id])
-    @posts = @user.posts.order(id: :desc)
+    # @user = User.find_by(id: params[:user_id])
+    @user = User.find(params[:user_id])
+
+    # @posts = @user.posts.includes(comments: [:author]).order(id: :desc)
+    # @posts = Post.includes([:author]).where(posts: { author_id: @user.id })
+    @posts = @user.posts
   end
 
   def show
-    @post = Post.find(params[:id])
-    # @comment = Comment.new
-    # @like = Like.new
+    @post = Post.includes(comments: [:author]).find(params[:id])
   end
 
   def new
