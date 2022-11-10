d1e6e7c8e4dc3194c43682882f1995704522b219
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-10 16:40:41 +0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, sign_out_via: [:get, :post]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "users#index"
+  # root to: "users#index"
+  post 'authenticate', to: 'authentication#authenticate'
 
   resources :users do
     resources :posts
