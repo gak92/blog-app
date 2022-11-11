aea1b2e462a577af00eedc7678e2dd98078bb037
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-11 17:41:24 +0200

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,14 @@ Rails.application.routes.draw do
     resources :likes
   end
 
+  namespace :api do
+    resources :users do 
+      resources :posts 
+    end 
+    resources :posts do
+       resources :comments 
+       resources :likes 
+    end
+  end
+
 end
