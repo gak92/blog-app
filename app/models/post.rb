17fc0e6cf9033267d4dd599787438e2a4041176c
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-11 12:20:25 +0500

    Add functionality to decrease number of post when delete post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,32 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   after_save :update_posts_counter
+  after_destroy :decrease_posts_counter
+  after_initialize :init
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
   def update_posts_counter
     author.increment!(:posts_counter)
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+  def decrease_posts_counter
+    author.decrement!(:posts_counter)
   end
+
+  def init
+    self.comments_counter ||= 0
+    self.likes_counter ||= 0
+  end
+  
 end
