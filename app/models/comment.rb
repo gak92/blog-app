a5aac8cb828e73e9e6ecaf5ca4945bf411cd56d3
gak92 <44802922+gak92@users.noreply.github.com>
2022-11-11 12:17:15 +0500

    Add the functionality to decrease counter when comment deleted

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,15 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   after_save :update_comments_counter
+  after_destroy :decrease_comments_counter
 
   def update_comments_counter
     post.increment!(:comments_counter)
   end
+
+  def decrease_comments_counter
+    post.decrement!(:comments_counter)
+  end
+
 end
